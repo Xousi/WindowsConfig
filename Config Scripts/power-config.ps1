0bddaa17e3140e370933cef3cf9913876d164253
Xousi <alexispetitjean@hotmail.fr>
2026-01-12 20:08:27 +0100

    finalise power config

--- Config Scripts/power-config.ps1
+++ Config Scripts/power-config.ps1
@@ -6,77 +6,139 @@ scope: power configs
     *
 #>
 
-Write-Output "----- checking for admin privileges"
-$isadmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-If ($isadmin -eq $False) {
-    Write-Output "----- admin privileges missing, restarting the script as admin"
-
-    Start-Process PowerShell -Verb runAs -ArgumentList "-noexit", "'$PSCommandPath'; pause"
+#Requires -RunAsAdministrator
 
 
-    # Exit
-}
-else {
-    Write-Output "----- running as admin, continuing"
-}
-pause
+# ----------------------------- Hibernate -----------------------------
 
-# to enable hibernate
-# powercfg -hibernate on
+# turn off Fast Startup
+$powerRegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+Set-ItemProperty -Path $powerRegPath -Name HiberbootEnabled -Value 0
 
-# # to turn off Fast Startup
-# # HKLM (HKEY_LOCAL_MACHINE)
-# REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /V HiberbootEnabled /T REG_dWORD /D 0 /F
+# enable the hibernate option in the control panel
+powercfg -hibernate on
 
-# # change timeout for monitor, sleep, hibernate & disk
+# show the hibernate option to the start/power menu
+$hibernateRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings"
+Set-ItemProperty -Path $hibernateRegPath -Name ShowHibernateOption -Value 1
 
 
-# # ac = plugged in
-# # dc = on battery
-# powercfg -x -monitor-timeout-ac 0
-# powercfg -x -monitor-timeout-dc 0
-# powercfg -x -standby-timeout-ac 0
-# powercfg -x -standby-timeout-dc 0
-# powercfg -x -hibernate-timeout-ac 0
-# powercfg -x -hibernate-timeout-dc 0
-# powercfg -x -disk-timeout-ac 0
-# powercfg -x -disk-timeout-dc 0
 
+
+# ----------------------------- Power Scheme -----------------------------
+
+# activate the Balanced power scheme
+powercfg /setactive SCHEME_BALANCED
+
+# turn off the display
+powercfg -x -monitor-timeout-ac 10 # 10 minutes plugged in
+powercfg -x -monitor-timeout-dc 5 # 5 minutes on battery
+# put the computer to sleep
+powercfg -x -standby-timeout-ac 30 # 30 minutes
+powercfg -x -standby-timeout-dc 15 # 15 minutes
+# put the computer to hibernate
+powercfg -x -hibernate-timeout-ac 0 # never
+powercfg -x -hibernate-timeout-dc 60 # 60 minutes
+# turn off the disk
+powercfg -x -disk-timeout-ac 20 # 20 minutes
+powercfg -x -disk-timeout-dc 10 # 10 minutes
+
+
+
+# ----------------------------- Power Button Actions -----------------------------
+
+# power button action
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS PBUTTONACTION 2 # hibernate
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS PBUTTONACTION 2 # hibernate
+# sleep button action
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS SBUTTONACTION 1 # sleep
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS SBUTTONACTION 1 # sleep
+# lid close action
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0 # do nothing
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0 # do nothing
+
+
+
+# ----------------------------- Testing & clarifications -----------------------------
 
 <#
-to check the current values of each power setting, you can
-    identify you current power scheme, and it's alias
-        run:
-            powercfg -query
-        first line will mention
-        '
-        Power Scheme GUID: [ID]  (Balanced)
-        GUID Alias: SCHEME_BALANCED
-        '
+##### check the current power settings & power scheme alias:
+powercfg -query
+    first line will mention
+    '
+    Power Scheme GUID: [ID]  (Balanced)
+    GUID Alias: SCHEME_BALANCED
+    '
 
-    check the config of each setting with:
-    *****
-    powercfg -query [scheme_id] [subgroup_id] [setting_id]
-    *****
+##### check the config of each setting with:
+powercfg -query [scheme_id] [subgroup_id] [setting_id]
 
-    for monitor timeout:
-    powercfg -query SCHEME_BALANCED SUB_SLEEP
+careful: settings are expressed in seconds, and hexadecimal format. (see below)
+# ac = plugged in
+# dc = on battery
 
-    for standby timeout:
-    powercfg -query SCHEME_BALANCED SUB_SLEEP STANDBYIDLE
+##### turn off the display
+powercfg -query SCHEME_BALANCED SUB_VIDEO VIDEOIDLE
+Current AC Power Setting Index: 0x00000258 = 600 seconds = 10 minutes
+Current DC Power Setting Index: 0x0000012c = 300 seconds = 5 minutes
+edit with:
+powercfg -x -monitor-timeout-ac 10 # 10 minutes
+powercfg -x -monitor-timeout-dc 5 # 5 minutes
 
-    for hibernate timeout:
-    powercfg -query SCHEME_BALANCED SUB_SLEEP HIBERNATEIDLE
+##### put the computer to sleep
+powercfg -query SCHEME_BALANCED SUB_SLEEP STANDBYIDLE
+    Current AC Power Setting Index: 0x00000708 = 1800 seconds = 30 minutes
+    Current DC Power Setting Index: 0x00000384 = 900 seconds = 15 minutes
+edit with:
+powercfg -x -standby-timeout-ac 30 # 30 minutes
+powercfg -x -standby-timeout-dc 15 # 15 minutes
 
-    for disk timeout:
-    powercfg -query SCHEME_BALANCED SUB_DISK DISKIDLE
+##### put the computer to hibernate
+powercfg -query SCHEME_BALANCED SUB_SLEEP HIBERNATEIDLE
+    Current AC Power Setting Index: 0x00000000 = disabled
+    Current DC Power Setting Index: 0x00000e10 = 3600 seconds = 60 minutes
+edit with:
+powercfg -x -hibernate-timeout-ac 0 # never
+powercfg -x -hibernate-timeout-dc 60 # 60 minutes
 
-    for button actions:
-    powercfg -query SCHEME_BALANCED
+##### turn off the disk
+powercfg -query SCHEME_BALANCED SUB_DISK DISKIDLE
+    Current AC Power Setting Index: 0x000004b0 = 1200 seconds = 20 minutes
+    Current DC Power Setting Index: 0x00000258 = 600 seconds = 10 minutes
+edit with:
+powercfg -x -disk-timeout-ac 20 # 20 minutes
+powercfg -x -disk-timeout-dc 10 # 10 minutes
 
 
-    to test
-    powercfg -query SCHEME_BALANCED SUB_BUTTONS
-    powercfg -query SCHEME_BALANCED SUB_DISK
-    powercfg -query SCHEME_BALANCED SUB_VIDEO VIDEOIDLE
+##### power button action
+### this setting is hidden, you can query it with -qh, or permanently unhide it with:
+powercfg -attributes SUB_BUTTONS PBUTTONACTION -ATTRIB_HIDE
+powercfg -qh SCHEME_BALANCED SUB_BUTTONS PBUTTONACTION
+    0 = Do nothing ; 1 = Sleep ; 2 = Hibernate ; 3 = Shut down ; 4 = Turn off the display
+    Current AC Power Setting Index: 0x00000002 = Hibernate
+    Current DC Power Setting Index: 0x00000002 = Hibernate
+edit with:
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS PBUTTONACTION 2 # hibernate
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS PBUTTONACTION 2 # hibernate
+
+
+##### sleep button action
+powercfg -qh SCHEME_BALANCED SUB_BUTTONS SBUTTONACTION
+    0 = Do nothing ; 1 = Sleep ; 2 = Hibernate ; 3 = Shut down ; 4 = Turn off the display
+    Current AC Power Setting Index: 0x00000001 = Sleep
+    Current DC Power Setting Index: 0x00000001 = Sleep
+edit with:
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS SBUTTONACTION 1 # sleep
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS SBUTTONACTION 1 # sleep
+
+##### lid close action
+powercfg -qh SCHEME_BALANCED SUB_BUTTONS LIDACTION
+    0 = Do nothing ; 1 = Sleep ; 2 = Hibernate ; 3 = Shut down ; 4 = Turn off the display
+    Current AC Power Setting Index: 0x00000001 = Sleep
+    Current DC Power Setting Index: 0x00000001 = Sleep
+edit with:
+powercfg -setacvalueindex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0 # do nothing
+powercfg -setdcvalueindex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0 # do nothing
+
+
 #>
